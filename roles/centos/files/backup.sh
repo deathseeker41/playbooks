c69828d454c2432783db5ca7aa79e6475501b548
NOXOiD <noxoid@deathsfolly.net>
2015-09-27 03:39:21 -0400

    update backup.sh params

--- roles/centos/files/backup.sh
+++ roles/centos/files/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #{{ ansible_managed }}
-rsync -avz --delete -e 'ssh -o StrictHostKeyChecking=no -i /root/.ssh/backup.key -p8001' /etc backup@home.deathsfolly.net:/data/backups/$(hostname)/
-rsync -avz --delete -e 'ssh -o StrictHostKeyChecking=no -i /root/.ssh/backup.key -p8001' /home backup@home.deathsfolly.net:/data/backups/$(hostname)/
+rsync -avz --delete -e 'ssh -o StrictHostKeyChecking=no -i /root/.ssh/backup.key -p8000' /etc backup@home.deathsfolly.net:/storage/backups/$(hostname)/
+rsync -avz --delete -e 'ssh -o StrictHostKeyChecking=no -i /root/.ssh/backup.key -p8000' /home backup@home.deathsfolly.net:/storage/backups/$(hostname)/
